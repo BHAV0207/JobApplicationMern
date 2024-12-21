93d5fc3991b3d644b2789b25b344334363161777
BHAV0207 <bhavyajain4274@gmail.com>
2024-12-21 13:50:58 +0530

    backend done

--- Backend/scrape_jobs.js
+++ Backend/scrape_jobs.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const axios = require("axios");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true });   
   const page = await browser.newPage();
 
   const diceURL =
