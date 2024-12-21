8a3ce9033e218a839be765e54dfe64d57655a0af
BHAV0207 <bhavyajain4274@gmail.com>
2024-12-21 09:38:03 +0530

    frontend code structure created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import JobList from "./Components/JobList";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import RegisterPage from "./Pages/RegisterPage";
+import LoginPage from "./Pages/LoginPage";
+import AdminPage from "./Pages/AdminPage";
+import UserPage from "./Pages/UserPage";
+import ProtectedRoutes from "./Components/ProtectedRoutes";
 
 function App() {
   return (
-    <div className="flex flex-col h-full bg-gray-100">
-      <Header></Header>
-      <JobList></JobList>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Header></Header>
+        <main>
+          <Routes>
+            <Route path="/" element={<LoginPage></LoginPage>} />
+            <Route path="/register" element={<RegisterPage></RegisterPage>}></Route>
+            <Route path="/admin" element={<ProtectedRoutes role='admin' element={<AdminPage></AdminPage>}/>} /> 
+            <Route path="/user" element={<ProtectedRoutes role='user' element={<AdminPage></AdminPage>}/>} />
+            <Route path="*" element={<Navigate to='/' replace/>}/>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
