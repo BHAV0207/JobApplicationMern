5e8febadc7679005b0a499fe0e2e905bb09e12af
BHAV0207 <bhavyajain4274@gmail.com>
2024-12-21 09:47:32 +0530

    protected rutes done

--- Frontend/src/Components/ProtectedRoutes.jsx
+++ Frontend/src/Components/ProtectedRoutes.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {jwtdecode} from 'jwt-decode'
+import { Navigate } from 'react-router-dom'
 
-function ProtectedRoutes() {
-  return (
-    <div>ProtectedRoutes</div>
-  )
+function ProtectedRoutes({role , element}) {
+
+  const token = localStorage.getItem('token');
+  if(!token) return <Navigate to={'/'} replace />
+
+  try{
+    const decode = jwtdecode('token');
+    if(decode.role != role) return <Navigate to={'/'} replace />
+
+    return element
+  }
+  catch(err){
+    return <Navigate to={'/'} replace />
+  }
 }
 
 export default ProtectedRoutes
